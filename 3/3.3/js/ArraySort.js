93a985f207b1a159e76e7de8f9ab6240cf021ea6
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-21 18:47:12 +0300

    Add method for sorting

--- 3/3.3/js/ArraySort.js
+++ 3/3.3/js/ArraySort.js
@@ -1,4 +1,40 @@
 "use strict";
 
 var array = ["1", "45", "6", "12", "4", "42", "8", "23"];
-document.write(array);
+
+
+var sorting = {
+
+
+    //methods
+    Bubble: function (array) {
+        var swap;
+        var x = array;
+        do {
+            swap = false;
+            for (var i=1; i < array.length; i++)
+            {
+                if (x[i] > x[i+1])
+                {
+                    var temp = x[i];
+                    x[i] = x[i+1];
+                    x[i+1] = temp;
+                    swap = true;
+                }
+            }
+            array.length--;
+        } while (swap);
+        return x;
+    }
+
+
+
+};
+
+
+sorting.Bubble();
+
+
+
+console.log('initial array:',array);
+console.log('Bubble sort:');
