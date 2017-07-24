f8cc9fe68f28a6e345efb6b11ed4102d6a0091cf
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-24 12:56:56 +0300

    fix array sorting

--- 3/3.3/js/ArraySort.js
+++ 3/3.3/js/ArraySort.js
@@ -2,39 +2,105 @@
 
 var array = ["1", "45", "6", "12", "4", "42", "8", "23"];
 
+var bubble = BubbleSort(array);
+var shell = ShellSort(array);
+var insert = InsertSort(array);
+var quick = QuickSort(array);
 
-var sorting = {
-
-
-    //methods
-    Bubble: function (array) {
-        var swap;
-        var x = array;
-        do {
-            swap = false;
-            for (var i=1; i < array.length; i++)
+function BubbleSort(array)
+{
+    let swap;
+    let n = array.length-1;
+    let x = array;
+    do {
+        swap = false;
+        for (let i=0; i < n; i++)
+        {
+            if (x[i] > x[i+1])
             {
-                if (x[i] > x[i+1])
+                let temp = x[i];
+                x[i] = x[i+1];
+                x[i+1] = temp;
+                swap = true;
+            }
+        }
+        n--;
+    } while (swap);
+    return x;
+}
+
+function InsertSort(array)
+{
+    for (let i = 1; i < array.length; i++)
+    {
+        if (array[i] < array[0])
+        {
+            array.unshift(array.splice(i,1)[0]);
+        }
+        else if (array[i] > array.length)
+        {
+            continue;
+        }
+        else {
+            for (let j = 1; j < i; j++) {
+                if (array[i] > array[j-1] && array[i] < array[j])
                 {
-                    var temp = x[i];
-                    x[i] = x[i+1];
-                    x[i+1] = temp;
-                    swap = true;
+                    array.splice(j,0,array.splice(i,1)[0]);
                 }
             }
-            array.length--;
-        } while (swap);
-        return x;
+        }
     }
+    return array;
+}
 
+function QuickSort(array) {
+    if (array.length <=1){
+        return array;
+    }
+    let arrayLength = array.length;
+    let position = Math.floor(arrayLength /2);
+    let value = array[position];
+    let less = [],
+        more = [],
+        same = [];
+    for (let i=0; i < arrayLength; i++){
+        if (array[i] === value){
+            same.push(array[i]);
+        }
+        else if (array[i] < value){
+            less.push(array[i]);
+        }
+        else if (array[i] > value){
+            more.push(array[i]);
+        }
+    }
+    return QuickSort(less).concat(same, QuickSort(more));
+}
 
+function ShellSort(arr){
 
-};
+    let len  = array.length;
+    let gapSize =  Math.floor(len/2);
 
+    while(gapSize > 0){
+        for(let i = gapSize; i < len; i++) {
 
-sorting.Bubble();
+            let temp = arr[i];
+            let j = i;
 
-
+            while(j >= gapSize && arr[j - gapSize] > temp) {
+                arr[j] = arr[j - gapSize];
+                j -= gapSize;
+            }
+            arr[j] = temp;
+        }
+        gapSize = Math.floor(gapSize/2);
+    }
+    return array;
+}
 
 console.log('initial array:',array);
-console.log('Bubble sort:');
+console.log('Bubble sort:',bubble);
+console.log('Insert sort:',insert);
+console.log('Quick sort:', quick);
+console.log('Shell sort:',shell);
