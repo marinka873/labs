7ac437e1690c0d8ce29c394aae02b50396c60770
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-25 09:53:11 +0300

    Fixes bug in task string calculator

--- 3/3.2/js/Polish_notation.js
+++ 3/3.2/js/Polish_notation.js
@@ -2,24 +2,68 @@ var expression = "3 4 * 2 5 + / 3 4 + *";
 
 var polska = new PolishNotation();
 
-function ConvertToPostfix() {
+var priority = function(operator){
+    if (operator === "^"){
+        return 4;
+    } else if ( operator === "*" && operator === "/"){
+        return 3;
+    } else if (operator === "+" && operator === "-"){
+        return 2;
+    } else if(operator === "(" && operator === ")") {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+function priorityOf(x) {
+    return priority[x] || 5;
+}
+
+function ConvertToPostfix(expression, dontPrint) {
     let postfixString = ' ';
     let infixString = new ConvertToPostfix();
+    let output = [];
+    let Stack = [];
 
-    let priority = function(operator){
-        if (operator === "^"){
-            return 3;
-        } else if ( operator === "*" && operator === "/"){
-            return 2;
-        } else if (operator === "+" && operator === "-"){
-            return 1;
-        } else{
-            return 0;
+
+    for (let i=1; i< expression.length; i++){
+        var x = expression[i];
+
+        if (x == "("){
+            Stack.push(x);
+        } else if (x == ")"){
+            while (Stack.pop() != "("){
+                output.push(Stack.pop());
+            }
+            Stack.pop();
         }
-    };
+        else {
+            while (Stack.length){
+                let visitedChar = Stack.pop();
 
+                let visitedCharpriority = priorityOf(visitedChar);
+                let currentChar = priorityOf(x);
+
+                if (visitedCharpriority >= currentChar){
+                    output.push(Stack.pop());
+                } else {
+                    break;
+                }
+            }
+            Stack.push(x);
+        }
+    }
+    while (Stack.length){
+        output.push(Stack.pop());
+
+        postfixString = output.join('');
+
+        !dontPrint && console.log(expression => postfixString);
+    }
+    return postfixString;
 
 }
+
 
 function PolishNotation() {
 
