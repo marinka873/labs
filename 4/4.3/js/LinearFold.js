1fe41bc18970ca22df6a5a69be2910ee2d1bef2d
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-27 16:24:42 +0300

    Added task with linear fold

---
+++ 4/4.3/js/LinearFold.js
@@ -0,0 +1,17 @@
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var linearFold = array.reduce(function (x, y){
+    return x+y;
+});
+var mulArray = array.reduce(function (x,y) {
+    return x*y;
+});
+
+var linearFoldVariantTwo = [[1,2], [3,4], [5,6], [7,8], [9,10]].reduce(function (x,y){
+    return x.concat(y);
+}) ;
+
+console.log('Array', array);
+console.log(linearFold);
+console.log(mulArray);
+console.log(linearFoldVariantTwo);
