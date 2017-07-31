a822ead8eaba38e4cc3700c2e4b7b098f89f229d
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-31 12:17:38 +0300

    Added task 3 LinearFold

--- 4/4.3/js/LinearFold.js
+++ 4/4.3/js/LinearFold.js
@@ -1,19 +1,25 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var linearFold = array.reduce(function (x, y){
-    return x+y;
-});
-var mulArray = array.reduce(function (x,y) {
-    return x*y;
-});
-
-var linearFoldVariantTwo = [[1,2], [3,4], [5,6], [7,8], [9,10]].reduce(function (x,y){
+var linearFold = [[1,2], [3,4], [5,6], [7,8], [9,10]].reduce(function (x,y){
     return x.concat(y);
-}) ;
+}, []);
+
+
+
+var foldLinear = function (array, callback, initialValue) {
+    var result = initialValue;
+    for (var i=0; i < array.length; i++){
+        result = callback.call(null, result, array[i]);
+    }
+    return result;
+};
+
+var arrayConcat = foldLinear(array, function (result, newArray) {
+    return result.concat(newArray);
+}, []);
 
 
 
 console.log('Array', array);
-console.log(linearFold);
-console.log(mulArray);
-console.log(linearFoldVariantTwo);
+console.log('Concat array with using ES5',linearFold);
+console.log('Concat array:', arrayConcat);
