c02cd7da3a139b10974b3693c829cf07ab801191
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-31 20:11:20 +0300

    Some changes in Unfold

--- 4/4.4/js/linearUnfold.js
+++ 4/4.4/js/linearUnfold.js
@@ -1,17 +1,32 @@
 
 
-function Unfold(element, callback, initialValue){
-    var array =[];
+var array = [100];
 
-    for (var i=element; i >0; --i){
-        array.push(i);
+// function Unfold(currentState){
+//
+//     for (var i = currentState; i > 0; --i){
+//         array.push(i);
+//     }
+//     return array;
+// }
+
+
+function Unfold (array, callback, initialValue) {
+    var result = initialValue;
+    for (var i=0; i < array.length; --i){
+        result = callback.call(null, result, array[i]);
     }
-    return array;
+    return result;
 }
+
+var reduce = Unfold(array, function ( currentState) {
+    var resultUnfold = currentState;
+    return [resultUnfold, currentState-1]
+});
 
 // var reduce = function (currentState) {
 //     var newValue = currentState/2;
 //     return [newValue, currentState -1]
 // };
 
-console.log(Unfold(100));
+console.log(Unfold(reduce(100)));
