9ac7b7d63bbdfb693d1c31abfc41a969cf80c559
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-31 12:23:34 +0300

    Added task 6 Filter

--- 4/4.6/js/Filter.js
+++ 4/4.6/js/Filter.js
@@ -3,4 +3,14 @@ var arr = [1, 33454544, 5, 6567, 4343, 123, -90, 78, 8];
 var arrFilter = arr.filter(function (number) {
     return number > 10;
 });
-console.log(arrFilter);
+console.log('Output number > 10 :',arrFilter);
+
+
+var filterArr =[];
+for (var i=0; i < arr.length; i++) {
+    if (arr[i] > 10){
+        filterArr.push(arr[i]);
+    }
+}
+
+console.log('Output filter number > 10 :', filterArr);
