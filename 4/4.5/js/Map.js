9bfde359e4f094c942194694715c3790c45bbd27
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-07-31 16:52:07 +0300

    bug fixes in task 5

--- 4/4.5/js/Map.js
+++ 4/4.5/js/Map.js
@@ -4,19 +4,19 @@ console.log('Origin array for transform with map ES6', array);
 console.log('Transform array with using Map ES6', transform);
 
 
-var arr = [2.1, 0.9, 3, 5, 4.6, 6, 45],
-    resultMap = [];
+var arr = [2.1, 0.9, 3, 5, 4.6, 6, 45];
 
 
 
-function Map(arr, callback) {
+function map(arr, callback) {
+    var resultMap =[];
     for (var i=0; i < arr.length; i++){
         resultMap.push(callback( arr[i], i))
     }
     return resultMap
 }
 
-var transformArr = Map(arr, Math.round);
+var transformArr = map(arr, Math.round);
 
 console.log('Origin array :', arr);
 console.log('Transform array:',transformArr);
