2c93e44398cf03a2105f9a9da5ea7eba8a7ad960
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-08-01 10:36:17 +0300

    Bug fixes in task 3 LinearFold

--- 4/4.3/js/LinearFold.js
+++ 4/4.3/js/LinearFold.js
@@ -4,7 +4,7 @@ var array = [[1,2], [3,4], [5,6], [7,8], [9,10]];
 var foldLinear = function (array, callback, initialValue) {
     var result = initialValue;
     for (var i=0; i < array.length; i++){
-        result = callback.call(null, result, array[i]);
+        result = callback(result, array[i]);
     }
     return result;
 };
