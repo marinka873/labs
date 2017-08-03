0f4649cfc0086bdacf27d5355b5766fdf29cd3e0
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-08-03 17:43:15 +0300

    Added inferface

---
+++ 5/js/jquery.registrationPage.js
@@ -0,0 +1,11 @@
+(function ($){
+    $.fn.registrationPage = function () {
+        var selectImage = document.getElementsByClassName('container-select__image--drop');
+        var dropArea = document.getElementsByClassName('container-drop__image');
+
+
+        console.log(dropArea);
+        console.log(selectImage);
+        console.log('Jquery plugin is work!');
+    };
+})(jQuery);
