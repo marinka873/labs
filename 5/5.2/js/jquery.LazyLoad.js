f7d5052a6b7a165747682141382428f308fb5167
ICX\maryna.tsimanovich <marina.timonovich96@gmail.com>
2017-08-11 10:31:09 +0300

    bug fixes

--- 5/5.2/js/jquery.LazyLoad.js
+++ 5/5.2/js/jquery.LazyLoad.js
@@ -3,56 +3,29 @@
 
     $.fn.LazyLoad = function () {
         let loadAPI = 'https://jsonplaceholder.typicode.com/photos';
-        let image = document.getElementsByTagName('img');
-        let imageGet = document.getElementsByClassName('.content-page__image0get');
 
 
-        $.ajax({
-            url: 'https://jsonplaceholder.typicode.com/photos',
-            dataType: "json",
-            success: function (){
-                console.log('loading API success');
-            }
-        });
-
-
-        $.getJSON(loadAPI, function (data) {
-            $.each(data, function (key, value) {
-                $('<img class=".content-page__image-get">').attr('src', value.url,'base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=').attr('data-src', value.thumbnailUrl).appendTo('.content-page__image');
-            });
-        });
-
-        function LazyLoadImage (){
-            for (let i=0; i< imageGet; i++){
-                if (imageGet[i].getAttribute('data-src')){
-                    imageGet[i].setAttribute('src', imageGet[i].getAttribute('data-src') );
-                    imageGet[i].removeAttribute('data-src');
-                }
+        function getImage(){
+            let n = 10;
+            for(let i=0; i < n; i++){
+                $.getJSON(loadAPI, function (data) {
+                    $.each(data.slice(0, 4), function (key, value) {
+                        $('<img>').addClass('content-page__image-get').attr('src', value.url).appendTo('.content-page__image');
+                    });
+                });
             }
         }
 
-        image.on('sroll', scrollPage);
+getImage();
 
-        function scrollPage(){
-            let currentHeigth = $(this).children('.content-page__image').height();
+       $('.content-page__image').scroll(function () {
+           let currentScroll = $(this)[0].scrollTop();
+           let maxScrollValue = $(this)[0].scrollHeight - $(this).height();
+           if (currentScroll >= maxScrollValue - 600){
+           }
 
-            if ($(this).scrollTop >= currentHeigth - $(this).height() - 100){
-                $(this).unbind('scroll');
-            }
-        }
+       });
 
-        // $(document).ready(function(){
-        //     $(window).scroll(function(){
-        //
-        //         if($(window).scrollTop() + $(window).height() >= $(document).height){
-        //             image.slideDown.show();
-        //         }
-        //     });
-        // });
-
-
-
-LazyLoadImage();
 };
 })(jQuery);
 
